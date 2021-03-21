4123d150466074bba31f5a3b918c846800ad1e53
David Sancho <dsnxmoreno@gmail.com>
2021-03-21 13:46:24 +0100

    chore: Remove package and yarn toplevel

--- demo/globals.js
+++ demo/globals.js
@@ -1,4 +1,3 @@
 joo_global_object.React = require("react");
 joo_global_object.ReactDOM = require("react-dom");
-/* joo_global_object.cx = require("@emotion/css").cx;
-joo_global_object.emotion = require("@emotion/react"); */
+joo_global_object.emotion = require("@emotion/css");
